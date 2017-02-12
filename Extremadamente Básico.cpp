d2d9fd1335ee92ede2c07437717c743a62a0ffb0
Aldair Bernal <aldair47x@Gmail.com>
2017-02-11 23:01:51 -0500

    Add files via upload

---
+++ Extremadamente Básico.cpp
@@ -0,0 +1,27 @@
+//---------***ENUNCIADO DEL PROBLEMA***---------//
+
+/*
+
+Leer 2 valores enteros y almacenarlos en variables, llamadas A y B , y sumarlas, asignando el resultado a la variable X. Mostrar X como se muestra abajo. No muestre ningún mensaje además de lo que se especifica y no olvide imprimir un salto de línea luego del resultado, de otra forma Ud. recibirá un “Presentation Error”.
+
+Entrada
+
+El archivo de entrada contiene 2 valores enteros.
+
+Salida
+
+Muestre la variable X de acuerdo al siguiente ejemplo, con un espacio en blanco antes y después del signo igual. 'X' está en mayúsculas y Ud. tiene que imprimir un espacio antes y después del signo '='.
+
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cin>>a;
+  cin>>b;
+  cout<<"X = "<<a+b<<endl;
+			
+	return 0;
+}
